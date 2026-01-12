79ae2e0e0f8806c18834262b7c6de9b34a1eed76
robovisioneer <shipit417@gmail.com>
2026-01-12 13:52:03 +0100

    KI alle Möglichkeiten durchspielen lassen

---
+++ groesserkleiner_ev.py
@@ -0,0 +1,18 @@
+def game(x):
+cd..    zahl_max = 100
+    zahl_min = 1
+    z = round((zahl_max - zahl_min)/2 + zahl_min, 0)
+    counter = 1
+    while x!=z:
+        if z > x:
+            zahl_max = z-1
+        elif z < x:
+            zahl_min = z+1
+        z = round((zahl_max - zahl_min)/2 + zahl_min, 0)
+        counter = counter + 1
+    return counter
+ev = 0
+for x in range(100):
+    ev = ev + game(x+1)
+ev=ev/100
+print(ev)
